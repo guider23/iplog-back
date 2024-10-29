c29c288692525bec6616a8fe66b14d292de90528
Z O R O <90545080+guider23@users.noreply.github.com>
2024-10-29 17:12:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 // Enable CORS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    exit(0);
+}
+
 
 // Database connection
 $servername = "sql12.freesqldatabase.com";  
